594902baabe52eb3d314bc9ac09087cd9d448722
rosalierazonable <rsrazonable1@up.edu.ph>
2021-06-04 07:47:56 +0800

    class representation for a job

--- src/com/rosalieraz/cmsc125/Job.java
+++ src/com/rosalieraz/cmsc125/Job.java
@@ -4,22 +4,20 @@ package com.rosalieraz.cmsc125;
  * JOB
  */
 
-import java.util.*;
-
 public class Job {
     final int id; // number of processes
-    int burst_time;
-    int size;
-    int waiting_time;
-    boolean is_assignable;
-    String status;
+    int size; // job size
+    int burst_time; // time the job will use the memory block
+    int waiting_time; // how long has it been waiting before it was assigned to the memory block
+    boolean is_assignable; // can the job be assigned to a block
+    String status; // free, assigned, done
 
     Job(int id, int burst_time, int size) {
-        this.waiting_time = 0;
         this.id = id;
-        this.burst_time = burst_time;
         this.size = size;
-        this.is_assignable = (size < 9500);
+        this.waiting_time = 0;
+        this.burst_time = burst_time;
+        this.is_assignable = (size < 9500); // the job should not exceed the largest memory block
         this.status = "free";
     }
 }
