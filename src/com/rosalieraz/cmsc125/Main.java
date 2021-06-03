3a78ba89a39a5ffb62526ebde2e49bbd67c9ff73
Rosalie Razonable <80081189+rosalierazonable@users.noreply.github.com>
2021-06-03 17:56:42 +0800

    Update Main.java

--- src/com/rosalieraz/cmsc125/Main.java
+++ src/com/rosalieraz/cmsc125/Main.java
@@ -1,8 +1,31 @@
 package com.rosalieraz.cmsc125;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        JobList job_list = new JobList();
+        Memory memory = new Memory();
+
+        System.out.println("Choose Algorithm: [1] Worst-Fit [2] Best Fit [3] First-Fit");
+        Scanner scanner= new Scanner(System.in);
+
+        int algo = scanner.nextInt();
+
+        if(algo == 1){
+            memory.sort_DESC();
+        } else if(algo == 2)
+            memory.sort_ASC();
+        else if(algo > 3) {
+            System.out.println("Invalid Choice of Algorithm");
+            return;
+        }
+
+
+        int time = 1;
+        while(!job_list.is_exhausted()) {
+            memory.allocate_blocks(job_list.get_job_list());
+        }
+        scanner.close();
     }
 }
