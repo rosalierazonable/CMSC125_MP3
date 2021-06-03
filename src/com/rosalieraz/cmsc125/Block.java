524bc896bf44aaf5ed980074e34e747a62475930
rosalierazonable <rsrazonable1@up.edu.ph>
2021-06-04 07:47:39 +0800

    class representation for memory block

--- src/com/rosalieraz/cmsc125/Block.java
+++ src/com/rosalieraz/cmsc125/Block.java
@@ -8,12 +8,14 @@ import java.util.*;
 
 public class Block {
     final int id;
-    int size;
-    Job job_occupant;
-    String status;
-    int job_count;
+    int size;  // memory size
+    int job_count; // count of jobs that the block has accommodated
+    int max_free_space; //max unused memory space
+
+    String status; // either free or occupied
     ArrayList<Integer> free_space = new ArrayList<>();
-    ArrayList<Integer> exhausted_space = new ArrayList<>();
+    ArrayList<Integer> exhausted_space = new ArrayList<>(); // list of exhausted space, its minimum corresponds to the heavily used/exhausted space
+    Job job_occupant;
 
     Block(int id, int size) {
         this.id = id;
@@ -27,12 +29,34 @@ public class Block {
         this.job_occupant = j;
         this.job_count++;
         this.status = "occupied";
-        this.exhausted_space.add(j.burst_time);
+        this.exhausted_space.add(j.size);
+        this.free_space.add(this.size - j.size);
     }
 
-    void kick_job_occupant() {
-        this.free_space.add((this.size - this.job_occupant.size));
+    void deallocate_block() {
         this.job_occupant = null;
         this.status = "free";
+    }
+
+    int calc_exhausted_space() {
+        Collections.sort(this.exhausted_space);
+        if(!this.exhausted_space.isEmpty())
+            return this.exhausted_space.get(0);
+        else
+            return -1;
+    }
+
+    void setMax_free_space() {
+        int temp = this.calc_exhausted_space();
+        if(temp > 0)
+            Collections.sort(this.free_space);
+    }
+
+    int get_total_internal_fragmentation() {
+        int sum = 0;
+        for (int internal_fragmentation: this.free_space) {
+            sum += internal_fragmentation;
+        }
+        return sum;
     }
 }
